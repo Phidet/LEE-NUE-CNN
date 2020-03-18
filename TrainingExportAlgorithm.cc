c1c312bb9ee9ac00f6e60cb02cc6966a0d155a1a
Jan Detje (MicroBooNE Mphil Student 2019) <jpd@pcmg.hep.phy.cam.ac.uk>
2020-03-18 14:16:47 +0000

    Initial project version

---
+++ TrainingExportAlgorithm.cc
@@ -0,0 +1,51 @@
+/** 
+*  @file   larpandoracontent/LArWorkshop/TrainingExportAlgorithm.cc 
+* 
+*  @brief  Implementation of the TrainingExport algorithm class. 
+* 
+*  $Log: $ */
+#include "Pandora/AlgorithmHeaders.h"
+#include "larpandoracontent/MyArea/TrainingExportAlgorithm.h"
+#include "PandoraMonitoringApi.h"
+#include "larpandoracontent/LArHelpers/LArPfoHelper.h"
+#include "larpandoracontent/LArHelpers/LArClusterHelper.h"
+#include "larpandoracontent/LArHelpers/LArInteractionTypeHelper.h"
+#include "larpandoracontent/LArHelpers/LArMonitoringHelper.h"
+
+#include <fstream>
+#include <stdlib.h>
+
+using namespace pandora;
+namespace lar_content
+{
+StatusCode TrainingExportAlgorithm::Run()
+{
+	const CaloHitList *pCaloHitListU(nullptr);
+	PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameU, pCaloHitListU));
+
+	CaloHitVector caloHitVectorU(pCaloHitListU->begin(), pCaloHitListU->end());
+	std::sort(caloHitVectorU.begin(), caloHitVectorU.end(), LArClusterHelper::SortHitsByPosition);
+	std::stringstream tempStr;
+	for (const CaloHit *const pCaloHit : caloHitVectorU)
+	{
+		tempStr << pCaloHit->GetPositionVector().GetX() << "," 
+				<< pCaloHit->GetPositionVector().GetZ() << ",";
+	}
+	std:string data = (tempStr.str()).pop_back();
+	system("python3 CNNHelper.py '"+data+"'"); // Calls the python program that does all the data handling and prints out the prediction to the terminal
+												// This is not an ideal implementation but implementing the data to image conversion in C++ would require more work  
+	
+return STATUS_CODE_SUCCESS;
+}
+//------------------------------------------------------------------------------------------------------------------------------------------
+StatusCode TrainingExportAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+// Read settings from xml file here
+//PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "PfoListName", m_pfoListName));
+PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameU", m_caloHitListNameU));
+PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameV", m_caloHitListNameV));
+PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameW", m_caloHitListNameW));
+
+return EventValidationAlgorithm::ReadSettings(xmlHandle);
+}
+} // namespace lar_content
