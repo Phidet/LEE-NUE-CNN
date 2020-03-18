dd625b14a76cdaee0e6fd8f2453b61707e4e0d3b
Phidet <jpd64@cam.ac.uk>
2020-03-18 17:39:33 +0000

    Implemented three layer readout.

--- TrainingExportAlgorithm.cc
+++ TrainingExportAlgorithm.cc
@@ -13,28 +13,114 @@
 #include "larpandoracontent/LArHelpers/LArMonitoringHelper.h"
 
 #include <fstream>
-#include <stdlib.h>
 
 using namespace pandora;
 namespace lar_content
 {
 StatusCode TrainingExportAlgorithm::Run()
 {
+std::cout<<"Before -1 ______________"<<std::endl;
+// ########## Taken from in parts from EventValidationAlgorithm.cc
+// This generates the interaction type (6 for CCQE E, 7 for CCQE E+P) from the reconstructable (!) particles
+
+	const MCParticleList *pMCParticleList(nullptr);
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pMCParticleList));
+
+    const CaloHitList *pCaloHitList(nullptr);
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pCaloHitList));
+
+    const PfoList *pPfoList(nullptr);
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pPfoList));
+
+	ValidationInfo validationInfo; //From EventValidationBaseAlgorithm
+	FillValidationInfo(pMCParticleList, pCaloHitList, pPfoList, validationInfo); //From EventValidationAlgorithm
+
+	//const LArMCParticleHelper::MCParticleToPfoHitSharingMap &mcToPfoHitSharingMap(validationInfo.GetInterpretedMCToPfoHitSharingMap());
+	MCParticleList associatedMCPrimaries;
+
+	MCParticleVector mcPrimaryVector;
+    LArMonitoringHelper::GetOrderedMCParticleVector({validationInfo.GetTargetMCParticleToHitsMap()}, mcPrimaryVector);
+
+	if(mcPrimaryVector.empty()) // End this run when there are no target MCParticles to process
+	return STATUS_CODE_SUCCESS;
+
+	for (const MCParticle *const pMCPrimary : mcPrimaryVector)
+    {
+        const bool hasMatch(mcToPfoHitSharingMap.count(pMCPrimary) && !mcToPfoHitSharingMap.at(pMCPrimary).empty());
+        const bool isTargetPrimary(validationInfo.GetTargetMCParticleToHitsMap().count(pMCPrimary));
+
+        if (!isTargetPrimary && !hasMatch)
+            continue;
+
+        associatedMCPrimaries.push_back(pMCPrimary);
+	}
+	const LArInteractionTypeHelper::InteractionType interactionType(LArInteractionTypeHelper::GetInteractionType(associatedMCPrimaries));
+	const int interactionTypeInt(static_cast<int>(interactionType));
+	// 	########## End of interaction type section
+
+	if(interactionTypeInt!=6 && interactionTypeInt!=7) 
+	return STATUS_CODE_SUCCESS;
+
 	const CaloHitList *pCaloHitListU(nullptr);
+	const CaloHitList *pCaloHitListV(nullptr);
+	const CaloHitList *pCaloHitListW(nullptr);
 	PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameU, pCaloHitListU));
+	PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameV, pCaloHitListV));
+	PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameW, pCaloHitListW));
+
+	const int entriesU = pCaloHitListU->size();
+	const int entriesV = pCaloHitListV->size();
+	const int entriesW = pCaloHitListW->size();
 
 	CaloHitVector caloHitVectorU(pCaloHitListU->begin(), pCaloHitListU->end());
-	std::sort(caloHitVectorU.begin(), caloHitVectorU.end(), LArClusterHelper::SortHitsByPosition);
-	std::stringstream tempStr;
+	CaloHitVector caloHitVectorV(pCaloHitListV->begin(), pCaloHitListV->end());
+	CaloHitVector caloHitVectorW(pCaloHitListW->begin(), pCaloHitListW->end());
+	//std::sort(caloHitVectorU.begin(), caloHitVectorU.end(), LArClusterHelper::SortHitsByPosition);
+	
+	int protonHits(0);
+	std::ostringstream tempStr; // Temporary storage for the data in case not enough proton hits are found and the event is dismissed 
 	for (const CaloHit *const pCaloHit : caloHitVectorU)
 	{
-		tempStr << pCaloHit->GetPositionVector().GetX() << "," 
-				<< pCaloHit->GetPositionVector().GetZ() << ",";
+		if(MCParticleHelper::GetMainMCParticle(pCaloHit)->GetParticleId() == 2212)
+			protonHits++;
+
+		tempStr << "," << pCaloHit->GetPositionVector().GetX() << "," << pCaloHit->GetPositionVector().GetZ();
 	}
-	std:string data = (tempStr.str()).pop_back();
-	system("python3 CNNHelper.py '"+data+"'"); // Calls the python program that does all the data handling and prints out the prediction to the terminal
-												// This is not an ideal implementation but implementing the data to image conversion in C++ would require more work  
-	
+
+	for(int i=0; i<2*(800-entriesU); i++) // Pads with zeros
+		tempStr << ",0.0";	
+// ---------------------------------
+	for (const CaloHit *const pCaloHit : caloHitVectorV)
+	{
+		if(MCParticleHelper::GetMainMCParticle(pCaloHit)->GetParticleId() == 2212)
+			protonHits++;
+
+		tempStr << "," << pCaloHit->GetPositionVector().GetX() << "," << pCaloHit->GetPositionVector().GetZ();
+	}
+
+	for(int i=0; i<2*(800-entriesV); i++) // Pads with zeros
+		tempStr << ",0.0";
+// ---------------------------------
+	for (const CaloHit *const pCaloHit : caloHitVectorW)
+	{
+		if(MCParticleHelper::GetMainMCParticle(pCaloHit)->GetParticleId() == 2212)
+			protonHits++;
+
+		tempStr << "," << pCaloHit->GetPositionVector().GetX() << "," << pCaloHit->GetPositionVector().GetZ();
+	}
+
+	for(int i=0; i<2*(800-entriesW); i++) // Pads with zeros
+		tempStr << ",0.0";
+// ----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
+	std::ofstream myfile;
+	myfile.open ("TrainingTestingOutput/TraTes.txt", std::ios::out | std::ios::app); // Appends all output to file   
+	myfile << interactionType-6 <<","<< protonHits; // 0 for shower only and 1 for shower + 1 proton track
+	myfile 	<< tempStr.str() << "\n";
+	myfile.close();
+	}
+
 return STATUS_CODE_SUCCESS;
 }
 //------------------------------------------------------------------------------------------------------------------------------------------
