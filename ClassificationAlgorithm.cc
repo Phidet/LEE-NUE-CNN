bd83f310116682b30edb5f72c033e4d7a925500b
Philip Detje <p.detje@t-online.de>
2020-04-16 13:01:13 +0100

    Revert "Ss image in pandora"

--- ClassificationAlgorithm.cc
+++ ClassificationAlgorithm.cc
@@ -1,13 +1,19 @@
 /** 
 *  @file   larpandoracontent/LArWorkshop/ClassificationAlgorithm.cc
 * 
-*  @brief  Implementation of the Classification algorithm class. 
+*  @brief  Implementation of the TrainingExport algorithm class. 
 * 
 *  $Log: $ */
 #include "Pandora/AlgorithmHeaders.h"
 #include "larpandoracontent/MyArea/ClassificationAlgorithm.h"
 #include "PandoraMonitoringApi.h"
+#include "larpandoracontent/LArHelpers/LArPfoHelper.h"
 #include "larpandoracontent/LArHelpers/LArClusterHelper.h"
+#include "larpandoracontent/LArHelpers/LArInteractionTypeHelper.h"
+#include "larpandoracontent/LArHelpers/LArMonitoringHelper.h"
+
+#include <fstream>
+#include <stdlib.h>
 
 
 using namespace pandora;
@@ -16,46 +22,23 @@ namespace lar_content
 
 StatusCode ClassificationAlgorithm::Run()
 {
-	std::cout << "Point0" <<std::endl;
 	const CaloHitList *pCaloHitListU(nullptr);
 	PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameU, pCaloHitListU));
-	std::cout << "Point0.1" <<std::endl;
-	const CaloHitList *pCaloHitListV(nullptr);
-	PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameV, pCaloHitListV));
-	std::cout << "Point0.2" <<std::endl;
-	const CaloHitList *pCaloHitListW(nullptr);
-	PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameW, pCaloHitListW));
-	std::cout << "Point0.3" << pCaloHitListU->size() << pCaloHitListV->size() << pCaloHitListW->size() <<std::endl;
+
 	CaloHitVector caloHitVectorU(pCaloHitListU->begin(), pCaloHitListU->end());
-	CaloHitVector caloHitVectorV(pCaloHitListV->begin(), pCaloHitListV->end());
-	CaloHitVector caloHitVectorW(pCaloHitListW->begin(), pCaloHitListW->end());
-	std::cout << "Point1" <<std::endl;
-	std::ostringstream tempStr;
-	(void) HitsToStringStream(caloHitVectorU, tempStr);
-	(void) HitsToStringStream(caloHitVectorV, tempStr);
-	(void) HitsToStringStream(caloHitVectorW, tempStr);
-
-	const std::string pathToHelper = "~/Pandora/PandoraPFA/LArContent-v03_15_04/larpandoracontent/MyArea/"; // Test setup to call a python helper function with the CNN
-	std::string data = tempStr.str();
-	data = data.substr(0, data.size()-1); // Removes last comma 																// TO DO: Import tensorflow model directly into C++ ... 
-	std::cout << "Calling CNN Python Helper" <<std::endl;
-	const std::string command = "python3 "+pathToHelper+"CNNHelper.py '"+ data + "'"; 											// ... and rasterize data here
-	system(command.c_str());
-
-	return STATUS_CODE_SUCCESS;
-}
-
-StatusCode ClassificationAlgorithm::HitsToStringStream(const CaloHitVector caloHitVector, std::ostringstream &tempStr)
-{
-	for (const CaloHit *const pCaloHit : caloHitVector)
+	std::sort(caloHitVectorU.begin(), caloHitVectorU.end(), LArClusterHelper::SortHitsByPosition);
+	std::stringstream tempStr;
+	for (const CaloHit *const pCaloHit : caloHitVectorU)
 	{
-		tempStr << pCaloHit->GetPositionVector().GetX() << "," << pCaloHit->GetPositionVector().GetZ() << ",";
+		tempStr << pCaloHit->GetPositionVector().GetX() << "," 
+				<< pCaloHit->GetPositionVector().GetZ() << ",";
 	}
-	for(int i=0; i<2*(500-caloHitVector.size()); i++) // Pads with zeros
-		tempStr << "0.0,";
-	return STATUS_CODE_SUCCESS;
+	std:string data = (tempStr.str()).pop_back();
+	system("python3 CNNHelper.py '"+data+"'"); // Calls the python program that does all the data handling and prints out the prediction to the terminal
+												// This is not an ideal implementation but implementing the data to image conversion in C++ would require more work  
+	
+return STATUS_CODE_SUCCESS;
 }
-
 //------------------------------------------------------------------------------------------------------------------------------------------
 StatusCode ClassificationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
@@ -65,6 +48,6 @@ PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle
 PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameV", m_caloHitListNameV));
 PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameW", m_caloHitListNameW));
 
-return STATUS_CODE_SUCCESS;
+return EventValidationAlgorithm::ReadSettings(xmlHandle);
 }
 } // namespace lar_content
--- TrainingExportAlgorithm.h
+++ TrainingExportAlgorithm.h
@@ -14,8 +14,6 @@
 //#include "PandoraMonitoringApi.h"
 //#endif
 
-#define IMSIZE 256 // Size of the generated image arrays 
-
 namespace lar_content
 {
 /** 
@@ -37,8 +35,7 @@ private:
 pandora::StatusCode Run();    
 pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 int InteractionType(const pandora::MCParticleList *const pMCParticleList, const pandora::CaloHitList *const pCaloHitList, const pandora::PfoList *const pPfoList);
-pandora::StatusCode PopulateImage(const pandora::CaloHitVector &caloHitVector, std::array<std::array<float,IMSIZE>,IMSIZE> &view, float (&label)[256][256][3]);
-pandora::StatusCode MinBoundaries(const pandora::CaloHitVector &caloHitVector, float &minX, float &minZ);
+pandora::StatusCode HitsToStringStream(const pandora::CaloHitVector caloHitVector, int &protonHits, std::ostringstream &tempStr);
 // Member variables here
 //std::string m_pfoListName;
 std::string m_caloHitListNameU;
