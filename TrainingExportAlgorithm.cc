07d242ffc53840f597f85ce1249103084fecf739
Phidet <jpd64@cam.ac.uk>
2020-06-08 13:21:14 +0200

    Version used for first UNet training

--- TrainingExportAlgorithm.cc
+++ TrainingExportAlgorithm.cc
@@ -33,64 +33,124 @@ namespace lar_content
 		//const CaloHitList *pCaloHitListU(nullptr);
 		const CaloHitList *pCaloHitListV(nullptr);
 		//const CaloHitList *pCaloHitListW(nullptr);
-		//PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameU, pCaloHitListU));
-		PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameV, pCaloHitListV));
-		//PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListNameW, pCaloHitListW));
+		
+		//PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_clusterListNames[0], pCaloHitListU));
+		std::cout<<"TTTTTTTTTTTTTTTTTT--Now "<<m_clusterListNames[1]<<std::endl;
+		PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_clusterListNames[1], pCaloHitListV));
+		//PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_clusterListNames[2], pCaloHitListW));
 
 		//CaloHitVector caloHitVectorU(pCaloHitListU->begin(), pCaloHitListU->end());
 		CaloHitVector caloHitVectorV(pCaloHitListV->begin(), pCaloHitListV->end());
 		//CaloHitVector caloHitVectorW(pCaloHitListW->begin(), pCaloHitListW->end());
 
 		const PfoList *pPfoList(nullptr);
+		//PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_pfoListNames[0], pPfoList));
 		PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pPfoList));
+
 
 		float minX(0);
 		float minZ(0);
 		float vertexX(std::numeric_limits<float>::max());
 		float vertexZ(std::numeric_limits<float>::max());
 
+		bool foundSuitableShower(false);
+
 		for (const ParticleFlowObject *const pPfo : *pPfoList) // Finds and adds shower to pfoListCrop
 		{
-			if (LArPfoHelper::IsShower(pPfo) && LArPfoHelper::IsNeutrinoFinalState(pPfo)) // && LArPfoHelper::IsNeutrinoFinalState(pPfo)
+			std::cout<<"? ";
+			if (LArPfoHelper::IsShower(pPfo))// && LArPfoHelper::IsNeutrinoFinalState(pPfo)) // && LArPfoHelper::IsNeutrinoFinalState(pPfo)
 			{	
+				std::cout<<"TrainingExportAlgorithm-------------- Found Shower X2"<<std::endl;
 				PfoList pfoListTemp; // This block skips reconstructed showers with fewer than 15 hits in the V plane
 				pfoListTemp.push_back(pPfo);
-				CaloHitList caloHitList;
-				LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW_V, caloHitList);
-				if(caloHitList.size()<15) continue;
-
-				CartesianVector v =  LArPfoHelper::GetVertex(pPfo)->GetPosition();
-				v = LArGeometryHelper::ProjectPosition(this->GetPandora(), v, TPC_VIEW_V); // Project 3D vertex onto 2D U view
-				
-				const float xDiff = v.GetX()-vertexX;
-				const float zDiff = v.GetZ()-vertexZ;
-				const float minSquaredSeperation = (IMSIZE*0.3*0.66)*(IMSIZE*0.3*0.66);
-
-				if(xDiff*xDiff+zDiff*zDiff>minSquaredSeperation)
+				CaloHitList caloHitListV;//caloHitListU, caloHitListV, caloHitListW;
+				//LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW_U, caloHitListU);
+				LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW_V, caloHitListV);
+				//LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW_W, caloHitListW);
+				//if(caloHitListU.size()<10 && caloHitListV.size()<10 && caloHitListW.size()<10) continue;
+				if(caloHitListV.size()>15)
 				{
-					vertexX = v.GetX();
-					vertexZ = v.GetZ();
-				    std::array<float, SEG>  hitXDensity= {0}; // Always combining 8 wires
-				    fillMinimizationArray(hitXDensity, pPfoList, pCaloHitListV, v, vertexX, vertexZ-IMSIZE/3*0.3, true);
-				    minX = findMin(hitXDensity, vertexX);
-
-					std::array<float, SEG>  hitZDensity= {0}; // Always combining 8 wires
-					fillMinimizationArray(hitZDensity, pPfoList, pCaloHitListV, v, vertexZ, minX, false);
-					minZ = findMin(hitZDensity, vertexZ);
-
-					// std::array<float, SEG>  hitX2Density= {0}; // Always combining 8 wires
-					// fillMinimizationArray(hitX2Density, pPfoList, pCaloHitListU, v, vertexX, minZ, true);
-					// minX = findMin(hitX2Density, vertexX);
-
-					PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateImage(caloHitVectorV, minX, minZ));
-					PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateRecoImage(*pPfoList, v));
+					foundSuitableShower=true;
+					break;
 				}
 			}
+		}
+		
+		if(foundSuitableShower)
+		{
+			std::cout<<"TrainingExportAlgorithm-------------- Point-1"<<std::endl;
+			const VertexList *pVertexList(nullptr);
+			PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pVertexList));
+			
+			if(pVertexList->size()>1 || pVertexList->size()==0)
+			{
+				std::cout<<"!!!!!!!!!!!TrainingExportAlgorithm Vertex Number: "<<pVertexList->size()<<std::endl;
+				return STATUS_CODE_FAILURE
+			}
+			CartesianVector vert =  pVertexList->front()->GetPosition();
+			
+			//CartesianVector vert =  LArPfoHelper::GetVertex(pPfo)->GetPosition();
+
+
+			// ///////////////////////////////////////////////////////////////////////////////////////
+			// /// U-view
+			// const CartesianVector vertU = LArGeometryHelper::ProjectPosition(this->GetPandora(), vert, TPC_VIEW_U); // Project 3D vertex onto 2D U view
+			
+			// vertexX = vertU.GetX();
+			// vertexZ = vertU.GetZ();
+		 //    std::array<float, SEG>  hitXDensity= {0}; // Always combining 8 wires
+		 //    fillMinimizationArray(hitXDensity, pPfoList, pCaloHitListV, vertU, vertexX, vertexZ-IMSIZE/3*0.3, true, TPC_VIEW_U);
+		 //    minX = findMin(hitXDensity, vertexX);
+
+			// std::array<float, SEG>  hitZDensity= {0}; // Always combining 8 wires
+			// fillMinimizationArray(hitZDensity, pPfoList, pCaloHitListV, vertU, vertexZ, minX, false, TPC_VIEW_U);
+			// minZ = findMin(hitZDensity, vertexZ);
+
+			// PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateImage(caloHitVectorU, minX, minZ));
+			// //PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateRecoImage(*pPfoList, vertV, TPC_VIEW_U));
+			
+
+			///////////////////////////////////////////////////////////////////////////////////////
+			/// V-view
+			std::cout<<"TrainingExportAlgorithm-------------- Point0"<<std::endl;
+			const CartesianVector vertV = LArGeometryHelper::ProjectPosition(this->GetPandora(), vert, TPC_VIEW_V); // Project 3D vertex onto 2D U view
+			
+			vertexX = vertV.GetX();
+			vertexZ = vertV.GetZ();
+		    std::array<float, SEG>  hitXDensity= {0}; // Always combining 8 wires
+		    fillMinimizationArray(hitXDensity, pPfoList, pCaloHitListV, vertV, vertexX, vertexZ-IMSIZE/3*0.3, true, TPC_VIEW_V);
+		    minX = findMin(hitXDensity, vertexX);
+
+			std::array<float, SEG>  hitZDensity= {0}; // Always combining 8 wires
+			fillMinimizationArray(hitZDensity, pPfoList, pCaloHitListV, vertV, vertexZ, minX, false, TPC_VIEW_V);
+			minZ = findMin(hitZDensity, vertexZ);
+
+			std::cout<<"TrainingExportAlgorithm-------------- Point1 "<<std::endl;
+			PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateImage(caloHitVectorV, minX, minZ));
+			PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateRecoImage(*pPfoList, vertV, TPC_VIEW_V));
+
+
+				// ///////////////////////////////////////////////////////////////////////////////////////
+				// /// W-view
+				// const CartesianVector vertW = LArGeometryHelper::ProjectPosition(this->GetPandora(), vert, TPC_VIEW_W); // Project 3D vertex onto 2D U view
+				
+				// vertexX = vertW.GetX();
+				// vertexZ = vertW.GetZ();
+			 //    std::array<float, SEG>  hitXDensity= {0}; // Always combining 8 wires
+			 //    fillMinimizationArray(hitXDensity, pPfoList, pCaloHitListV, vertW, vertexX, vertexZ-IMSIZE/3*0.3, true, TPC_VIEW_W);
+			 //    minX = findMin(hitXDensity, vertexX);
+
+				// std::array<float, SEG>  hitZDensity= {0}; // Always combining 8 wires
+				// fillMinimizationArray(hitZDensity, pPfoList, pCaloHitListV, vertW, vertexZ, minX, false, TPC_VIEW_W);
+				// minZ = findMin(hitZDensity, vertexZ);
+
+				// PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateImage(caloHitVectorW, minX, minZ));
+				// //PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PopulateRecoImage(*pPfoList, vertV, TPC_VIEW_W));
 		}
 		return STATUS_CODE_SUCCESS;
 	}
 
-	void TrainingExportAlgorithm::fillMinimizationArray(std::array<float, SEG> &hitDensity, const PfoList *const pPfoList, const CaloHitList *const pCaloHitList, const CartesianVector v, const float startD1, const float startD2, const bool directionX)
+	void TrainingExportAlgorithm::fillMinimizationArray(std::array<float, SEG> &hitDensity, const PfoList *const pPfoList, const CaloHitList *const pCaloHitList, const CartesianVector v, const float startD1, const float startD2, const bool directionX, const HitType TPC_VIEW)
 	{
 		float weight, d1, d2;
 
@@ -110,7 +170,7 @@ namespace lar_content
 			try
 			{
 				CartesianVector v2 =  LArPfoHelper::GetVertex(pPfo)->GetPosition();
-				v2 = LArGeometryHelper::ProjectPosition(this->GetPandora(), v2, TPC_VIEW_V); // Project 3D vertex onto 2D U view
+				v2 = LArGeometryHelper::ProjectPosition(this->GetPandora(), v2, TPC_VIEW); // Project 3D vertex onto 2D view
 				const float xDiff = v.GetX()-v2.GetX();
 				const float zDiff = v.GetZ()-v2.GetZ();
 				const float squaredDist = xDiff*xDiff+zDiff*zDiff;
@@ -126,7 +186,7 @@ namespace lar_content
 			PfoList pfoListTemp;
 			pfoListTemp.push_back(pPfo);
 			CaloHitList caloHitList;
-			LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW_V, caloHitList);
+			LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW, caloHitList);
 			for (const CaloHit *const pCaloHit : caloHitList)
 			{
 				if(directionX){
@@ -214,7 +274,7 @@ namespace lar_content
 
 	StatusCode TrainingExportAlgorithm::PopulateImage(const CaloHitVector &caloHitVector, const float minX, const float minZ)
 	{
-		std::ofstream file("OutTest/viewV.bin", std::ios::out | std::ios::binary | std::ios::app); 
+		std::ofstream file("OutTest/viewV2.bin", std::ios::out | std::ios::binary | std::ios::app); 
 		if(!file)
 		{
 			std::cout<<"Problem opening/creating binary file in TrainingExportAlgorithm::PopulateImage."<<std::endl;
@@ -225,18 +285,15 @@ namespace lar_content
 		file.write((char*)&hitNumber, sizeof(hitNumber));
 		file.write((char*)&minX, sizeof(minX));
 		file.write((char*)&minZ, sizeof(minZ));
-
 		for (const CaloHit *const pCaloHit : caloHitVector)
 		{
 			const float x = pCaloHit->GetPositionVector().GetX();
 			const float z = pCaloHit->GetPositionVector().GetZ();
 			if((x-minX)/0.3>=IMSIZE || (z-minZ)/0.3>=IMSIZE || (x-minX)<0 || (z-minZ)<0) continue; // Skipps hits that are not in the crop area
-			
 			std::array<float, 6> pixel = {0};
 			pixel[0] = x;
 			pixel[1] = z; 
 			pixel[2] = pCaloHit->GetHadronicEnergy(); // Populates input image
-
 			const MCParticleWeightMap  &mcParticleWeightMap(pCaloHit->GetMCParticleWeightMap());
 			// Populates prediction image
 			for (const MCParticleWeightMap::value_type &mapEntry : mcParticleWeightMap)
@@ -260,9 +317,9 @@ namespace lar_content
 	}	
 
 
-	StatusCode TrainingExportAlgorithm::PopulateRecoImage(const PfoList &pfoList, const CartesianVector v)
+	StatusCode TrainingExportAlgorithm::PopulateRecoImage(const PfoList &pfoList, const CartesianVector v, const HitType TPC_VIEW)
 	{
-		std::ofstream file("OutTest/PandoraRecoV.bin", std::ios::out | std::ios::binary | std::ios::app); 
+		std::ofstream file("OutTest/PandoraRecoV2.bin", std::ios::out | std::ios::binary | std::ios::app); 
 		if(!file)
 		{
 			std::cout<<"Problem opening/creating binary file in TrainingExportAlgorithm::PopulateImage."<<std::endl;
@@ -282,7 +339,7 @@ namespace lar_content
 			PfoList pfoListTemp;
 			pfoListTemp.push_back(pPfo);
 			CaloHitList caloHitList;
-			LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW_V, caloHitList);
+			LArPfoHelper::GetCaloHits(pfoListTemp, TPC_VIEW, caloHitList);
 			for (const CaloHit *const pCaloHit : caloHitList)
 			{
 				std::array<float, 4> pixel = {0};
@@ -325,10 +382,24 @@ namespace lar_content
 	StatusCode TrainingExportAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 	{
 	// Read settings from xml file here
-	//PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "PfoListName", m_pfoListName));
-		PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameU", m_caloHitListNameU));
-		PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameV", m_caloHitListNameV));
-		PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListNameW", m_caloHitListNameW));
+	// PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "PfoListNames", m_pfoListNames));
+
+	PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
+        "PfoListNames", m_pfoListNames));
+	PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
+        "CaloHitListNames", m_clusterListNames));
+
+    if (m_pfoListNames.empty())
+    {
+        std::cout << "TrainingExportAlgorithm::ReadSettings - Must provide names of Pfo lists for use in U-Net." << std::endl;
+        return STATUS_CODE_INVALID_PARAMETER;
+    }
+
+    if (m_clusterListNames.empty())
+    {
+        std::cout << "TrainingExportAlgorithm::ReadSettings - Must provide names of cluster lists for use in U-Net." << std::endl;
+        return STATUS_CODE_INVALID_PARAMETER;
+    }
 
 		return STATUS_CODE_SUCCESS;
 	}
